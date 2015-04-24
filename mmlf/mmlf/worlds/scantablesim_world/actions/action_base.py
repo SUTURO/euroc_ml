29e0b777000546aacdbafc6a16074e2f6926a054
stelter <stelter@tzi.de>
2015-04-24 12:32:59 +0200

    added new actions

--- mmlf/mmlf/worlds/scantablesim_world/actions/action_base.py
+++ mmlf/mmlf/worlds/scantablesim_world/actions/action_base.py
@@ -7,3 +7,24 @@ class ActionBase(object):
     @abc.abstractmethod
     def __call__(self, environment):
         pass
+
+class SimSimAction(object):
+
+    def __init__(self):
+        pass
+
+    def moveLeft(self, env):
+        env.move_to(self.pos_x, self.pos_y)
+        pass
+
+    def moveRight(self, env):
+        pass
+
+    def moveUp(self, env):
+        pass
+
+    def moveDown(self, env):
+       pass
+
+    def scan(self, env):
+        pass
