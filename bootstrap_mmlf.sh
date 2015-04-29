da83f3b85a49f85059e63af026079f2be90a4b3f
EurocUser <Euroc@User>
2015-04-29 17:24:10 +0200

    bootstrap verbessert

--- bootstrap_mmlf.sh
+++ bootstrap_mmlf.sh
@@ -3,13 +3,13 @@ echo "Installing global dependencies"
 sudo apt-get install python2.7 python-virtualenv wget tar make
 
 echo "Creating and entering virtualenv (mmlf_venv)"
-python2 -m virtualenv --always-copy mmlf_venv
-source mmlf_venv/bin/activate
+#python2 -m virtualenv --always-copy mmlf_venv
+#source mmlf_venv/bin/activate
 
 echo "DEBUG: " $PS1 
 
 echo "Building local sip and PyOt4"
-cd mmlf_venv
+#cd mmlf_venv
 mkdir packages
 cd packages
 
@@ -18,10 +18,10 @@ wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.16.7/sip-4.16.7.tar.gz
 tar xfz sip-4.16.7.tar.gz
 cd sip-4.16.7
 echo "-- Building sip"
-../../bin/python configure.py --incdir=../../include 
+python configure.py --incdir=../../include 
 make || exit $?
 echo "-- Installing sip"
-make install || exit $?
+sudo make install || exit $?
 cd ..
 
 echo "-- Downloading PyQt4"
@@ -29,15 +29,15 @@ wget http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.3/PyQt-x11-gpl-4
 tar xfz PyQt-x11-gpl-4.11.3.tar.gz
 cd PyQt-x11-gpl-4.11.3
 echo "-- Building PyQt4"
-../../bin/python configure.py --confirm-license 
+python configure.py --confirm-license 
 make || exit $?
 echo "-- Installing PyQt4"
-make install || exit $?
+sudo make install || exit $?
 cd ../../../
 
 echo "Installing MMLF dependencies"
-mmlf_venv/bin/pip install pyyaml matplotlib numpy
-mmlf_venv/bin/pip install scipy
+sudo pip install pyyaml matplotlib numpy
+sudo pip install scipy
 
 echo "Done"
 
