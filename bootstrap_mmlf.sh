1eada938f6c2787b163827aba112d82beea4491a
Torben Hansing <hansa@tzi.de>
2015-05-03 13:47:14 +0000

    Boostrap script angepasst fuer native Installation

--- bootstrap_mmlf.sh
+++ bootstrap_mmlf.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 echo "Installing global dependencies"
-sudo apt-get install python2.7 python-virtualenv wget tar make libyaml-dev
+sudo apt-get install python2.7 wget tar make libyaml-dev swig
 
-echo "Creating and entering virtualenv (mmlf_venv)"
-python2 -m virtualenv --always-copy mmlf_venv
-source mmlf_venv/bin/activate
-
-echo "DEBUG: " $PS1 
+#echo "Creating and entering virtualenv (mmlf_venv)"
+#python2 -m virtualenv --always-copy mmlf_venv
+#source mmlf_venv/bin/activate
 
 echo "Building local sip and PyOt4"
-cd mmlf_venv
+#cd mmlf_venv
 mkdir packages
 cd packages
 
@@ -18,10 +16,10 @@ wget http://sourceforge.net/projects/pyqt/files/sip/sip-4.16.7/sip-4.16.7.tar.gz
 tar xfz sip-4.16.7.tar.gz
 cd sip-4.16.7
 echo "-- Building sip"
-../../bin/python configure.py --incdir=../../include 
+python2 configure.py --incdir=../../include 
 make || exit $?
 echo "-- Installing sip"
-make install || exit $?
+sudo make install || exit $?
 cd ..
 
 echo "-- Downloading PyQt4"
@@ -29,18 +27,18 @@ wget http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.3/PyQt-x11-gpl-4
 tar xfz PyQt-x11-gpl-4.11.3.tar.gz
 cd PyQt-x11-gpl-4.11.3
 echo "-- Building PyQt4"
-../../bin/python configure.py --confirm-license 
+python2 configure.py --confirm-license 
 make || exit $?
 echo "-- Installing PyQt4"
-make install || exit $?
-cd ../../../
+sudo make install || exit $?
+cd ../../
 
 echo "Installing MMLF dependencies"
-mmlf_venv/bin/pip install pyyaml numpy 
-mmlf_venv/bin/pip install matplotlib scipy
+python2 -m pip install pyyaml numpy 
+python2 -m pip install matplotlib scipy
 
 echo "Installing libANN and scikits.ann"
-cd mmlf_venv/packages
+cd packages
 
 echo "-- Downloading libANN"
 wget http://www.cs.umd.edu/~mount/ANN/Files/1.1.2/ann_1.1.2.tar.gz
@@ -48,7 +46,7 @@ tar xfz ann_1.1.2.tar.gz
 cd ann_1.1.2
 echo "-- Building libANN"
 make linux-g++ || exit $?
-cp -r bin/ lib/ include/ ../../
+sudo cp -r bin/ lib/ include/ /
 cd ..
 
 echo "-- Downloading scikits.ann"
@@ -61,8 +59,8 @@ sed '/NumpyTest/s/^/#/g' -i ./scikits/ann/__init__.py
 sed '/NumpyTest/s/^/#/g' -i ./scikits/ann/tests/test_ann.py
 
 echo "-- Building scikits.ann"
-ANN_LIB=../../lib ANN_INCLUDE=../../include ../../bin/python setup.py build_ext --inplace build test || exit $?
-../../bin/python setup.py install
+ANN_LIB=/lib ANN_INCLUDE=/include python2 setup.py build_ext --inplace build test || exit $?
+sudo python2 setup.py install
 
 echo "Done"
 
