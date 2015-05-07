81a9afe6bcbcb873851fec7277ee9cfc0d842bdd
EurocUser <Euroc@User>
2015-05-07 15:23:35 +0200

    bux fixes + new rewards

--- mmlf/mmlf/worlds/gazo_scan_table/actions/actions.py
+++ mmlf/mmlf/worlds/gazo_scan_table/actions/actions.py
@@ -17,7 +17,9 @@ class GazeboActions(object):
         self.__maxPan = maxPan
         self.__minTilt = minTilt
         self.__maxTilt = maxTilt
-        self.__move_cam = rospy.ServiceProxy("/suturo/manipulation/move_mastcam", MoveMastCam)
+        self.oldy = -1
+        self.oldx = -1
+        self.move_cam = rospy.ServiceProxy("/suturo/manipulation/move_mastcam", MoveMastCam)
         self.__add_pointcloud = rospy.ServiceProxy("/suturo/environment/add_point_cloud", AddPointCloud)
 
     def performAction(self, action, env):
@@ -33,51 +35,60 @@ class GazeboActions(object):
             return self.scan(env)
 
     def __move(self, x, y, env):
+        reward = -1
         env.pan += x
         env.tilt += y
         if env.pan < self.__minPan:
             env.pan = self.__minPan
+            reward = -1
         elif env.pan > self.__maxPan:
             env.pan = self.__maxPan
-
+            reward = -1
         if env.tilt < self.__minTilt:
             env.tilt = self.__minTilt
+            reward = -1
         elif env.tilt > self.__maxTilt:
             env.tilt = self.__maxTilt
+            reward = -1
+        self.move_cam(pan=env.pan, tilt=env.tilt)
         env.update_index()
-        #self.__move_cam(pan=env.pan, tilt=env.tilt)
+        return reward
 
     def moveLeft(self, environment):
-        self.__move(-self.__pan_step, 0, environment)
-        return -1
+        return self.__move(-self.__pan_step, 0, environment)
     moveLeft.name = "Move Left"
 
     def moveRight(self, env):
-        self.__move(self.__pan_step, 0, env)
-        return -1
+        return self.__move(self.__pan_step, 0, env)
     moveRight.name = "Move Right"
 
     def moveUp(self, env):
-        self.__move(0, -self.__tilt_step, env)
-        return -1
+        return self.__move(0, -self.__tilt_step, env)
     moveUp.name = "Move Up"
 
     def moveDown(self, env):
-        self.__move(0, self.__tilt_step, env)
-        return -1
+        return self.__move(0, self.__tilt_step, env)
     moveDown.name = "Move Down"
 
     def scan(self, env):
         old = env.discovered_percentage
+        x,y = env.camera_index
+        # Scan the image and add to the point cloud\
         # Move the cam
-        if self.__move_cam(pan=env.pan, tilt=env.tilt):
-            # Scan the image and add to the point cloud
+        if self.oldx ==x and self.oldy == y:
+            return -10
+        elif self.move_cam(pan=env.pan, tilt=env.tilt):
+            r = 20 if not env.currentState["isScanned"] else -10
+            env.environmentLog.info("state: %s, Scanned: %d, x: %d, y: %d" % (str(env.currentState), r, x, y))
             self.__add_pointcloud(scenecam=True)
             env.update_map()
-            new = env.discovered_percentage
-            diff = int(new) - int(old)
-            if diff <= 3:
-                return -10
-            else:
-                return 10
+            self.oldx, self.oldy = x,y
+            return r
+        return -10
+            #          new = env.discovered_percentage
+            #           diff = int(new) - int(old)
+            #if diff <= 3:
+        #                return -8
+        #            else:
+        #                return 10
     scan.name = "Scan"
--- mmlf/mmlf/worlds/gazo_scan_table/environments/gazebo_scan_table_environment.py
+++ mmlf/mmlf/worlds/gazo_scan_table/environments/gazebo_scan_table_environment.py
@@ -6,7 +6,6 @@ from geometry_msgs.msg._PoseStamped import PoseStamped
 import rospy
 from suturo_environment_msgs.srv import GetMap, ResetMap, GetPercentCleared
 from std_msgs import msg as std_msgs
-import time
 from mmlf.framework.protocol import EnvironmentInfo
 from mmlf.environments.single_agent_environment import SingleAgentEnvironment
 from mmlf.framework.spaces import StateSpace, ActionSpace
@@ -14,9 +13,9 @@ from mmlf.worlds.gazo_scan_table.actions import GazeboActions
 from suturo_planning_search.cell import Cell
 from suturo_planning_manipulation.transformer import Transformer
 from suturo_planning_search.map import Map
-from suturo_planning_yaml_pars0r.yaml_pars0r import YamlPars0r
 from mmlf_msgs.srv import TransformerService, TransformerServiceResponse
-
+from mmlf.framework.observables import FloatStreamObservable
+import time
 
 __author__ = 'hansa'
 
@@ -46,8 +45,8 @@ class MMLFNode(Process):
 class GazeboScanTableEnvironment(SingleAgentEnvironment):
 
     DEFAULT_CONFIG_DICT = {
-        "minPan": -1.10,
-        "maxPan": 1.20,
+        "minPan": -0.65,
+        "maxPan": 0.6,
         "minTilt": 0.50,
         "maxTilt": 1.40,
         "stepsPan": 4,
@@ -75,15 +74,29 @@ class GazeboScanTableEnvironment(SingleAgentEnvironment):
         self.__percent_cleared = 0.0
         self.__update_index = True
         self.__map = None
+        self.__reward = 0
+        self.__scans = 0
         self.pan = 0
         self.tilt = 0.5
+        self.scanObservable = FloatStreamObservable(title='%s Scans' % self.__class__.__name__,
+                                  time_dimension_name='Episode',
+                                  value_name='Number of Scans')
+
+        self.get_map = rospy.ServiceProxy(Map.NAME_SERVICE_GET_MAP, GetMap)
+        self.reset_map = rospy.ServiceProxy(Map.NAME_SERVICE_RESET_MAP, ResetMap)
+        self.map_percent_cleared = rospy.ServiceProxy("/suturo/environment/get_map_percent_cleared", GetPercentCleared)
+        self.__transform_to = rospy.ServiceProxy("/mmlf/transform_to", TransformerService)
+        # Initialize the simulation
+        self.__init_simulation()
 
         oldStyleStateSpace = {
-            "isScanned": ("discrete", [0, 1]),
-            "isSomethingRight": ("discrete", [0,1]),
-            "isSomethingLeft": ("discrete", [0,1]),
-            "isSomethingUp": ("discrete", [0,1]),
-            "isSomethingDown": ("discrete", [0,1])
+           # "x": ("discrete", [0, self.rows]),
+           # "y": ("discrete", [0, self.columns]),
+            "isScanned": ("discrete", [True, False]),
+            "isLeftDown": ("discrete", [True, False]),
+            "isRightDown": ("discrete", [True, False]),
+            "isLeftUp": ("discrete", [True, False]),
+            "isRightUp": ("discrete", [True, False])
         }
         self.stateSpace = StateSpace()
         self.stateSpace.addOldStyleSpace(oldStyleStateSpace, limitType="soft")
@@ -98,14 +111,17 @@ class GazeboScanTableEnvironment(SingleAgentEnvironment):
             "action": ("discrete", self.__actions.actions)
         }
 
+        x, y = self.camera_index
         self.actionSpace = ActionSpace()
         self.actionSpace.addOldStyleSpace(oldStyleActionSpace)
         self.initialState = {
-            "isScanned": 0,
-            "isSomethingRight": 1,
-            "isSomethingLeft": 0,
-            "isSomethingUp": 1,
-            "isSomethingDown": 0
+         #   "x": x,
+         #   "y": y,
+            "isScanned": False,
+            "isLeftDown": True,
+            "isRightDown": True,
+            "isLeftUp": True,
+            "isRightUp": True
         }
         self.currentState = deepcopy(self.initialState)
         if useGUI:
@@ -113,41 +129,54 @@ class GazeboScanTableEnvironment(SingleAgentEnvironment):
             from mmlf.worlds.gazo_scan_table.environments.gazebo_table_viewer import GazeboTableViewer
             VIEWERS.addViewer(lambda: GazeboTableViewer(self), "GazeboTable Viewer")
 
-        self.get_map = rospy.ServiceProxy(Map.NAME_SERVICE_GET_MAP, GetMap)
-        self.reset_map = rospy.ServiceProxy(Map.NAME_SERVICE_RESET_MAP, ResetMap)
-        self.map_percent_cleared = rospy.ServiceProxy("/suturo/environment/get_map_percent_cleared", GetPercentCleared)
-        self.__transform_to = rospy.ServiceProxy("/mmlf/transform_to", TransformerService)
-        # Initialize the simulation
-        self.__init_simulation()
+    def check_range(self,r=1):
+        result = False
+        posx, posy = self.camera_index
+        for y in range(max(0, posy-r),min(posy+r,self.columns)):
+            for x in range(max(0,posx-r),min(posx+r, self.rows)):
+                result = result or  not self.__cell_map[x,y]
+        return result
 
     def check_new_state(self):
-        x, y = self.camera_index
-        self.currentState["isSomethingRight"] = False
-        for i in range(x+1, self.rows):
-            if not self.cell_map[i,y]:
-                self.currentState["isSomethingRight"] = True
+        posx, posy = self.camera_index
+        self.currentState["isLeftDown"] = False
+        for y in range(posy):
+            for x in range(posx):
+                if not self.__cell_map[x, y]:
+                    self.currentState["isLeftDown"] = True
+                    break
+            if self.currentState["isLeftDown"]:
                 break
-        self.currentState["isSomethingLeft"] = False
-        for i in range(0, x):
-            if not self.cell_map[i,y]:
-                self.currentState["isSomethingLeft"] = True
+        self.currentState["isRightDown"] = False
+        for y in range(posy):
+            for x in range(posx+1, self.rows):
+                if not self.__cell_map[x, y]:
+                    self.currentState["isRightDown"] = True
+                    break
+            if self.currentState["isRightDown"]:
                 break
-        self.currentState["isSomethingUp"] = False
-        for i in range(y+1, self.columns):
-            if not self.cell_map[x,i]:
-                self.currentState["isSomethingUp"] = True
+        self.currentState["isLeftUp"] = False
+        for y in range(posy+1, self.columns):
+            for x in range(posx):
+                if not self.__cell_map[x, y]:
+                    self.currentState["isLeftUp"] = True
+                    break
+            if self.currentState["isLeftUp"]:
                 break
-        self.currentState["isSomethingDown"] = False
-        for i in range(0, y):
-            if not self.cell_map[x,i]:
-                self.currentState["isSomethingDown"] = True
+        self.currentState["isRightUp"] = False
+        for y in range(posy+1, self.columns):
+            for x in range(posx+1, self.rows):
+                if not self.__cell_map[x, y]:
+                    self.currentState["isRightUp"] = True
+                    break
+            if self.currentState["isRightUp"]:
                 break
 
     def getInitialState(self):
         return deepcopy(self.initialState)
 
     def _checkEpisodeFinished(self):
-        return self.discovered_percentage >= self.configDict["percentCleared"]
+        return self.discovered_percentage >= self.configDict["percentCleared"] or self.stepCounter >= 500
 
 
     def __init_simulation(self):
@@ -156,7 +185,7 @@ class GazeboScanTableEnvironment(SingleAgentEnvironment):
         self.__stop_publisher = Value("b", False)
         self.__publisher = MMLFNode(description.description_yaml, self.__stop_publisher)
         self.__publisher.start()
-        task = YamlPars0r.parse_yaml(description.description_yaml)
+        time.sleep(2)
 
     def update_index(self):
         self.__update_index = True
@@ -182,40 +211,41 @@ class GazeboScanTableEnvironment(SingleAgentEnvironment):
         action = actionObject["action"]
         previousState = deepcopy(self.currentState)
         x, y = self.pan, self.tilt
+        posx, posy = self.camera_index
         reward = self.__actions.performAction(action, self)
 
         if action == "scan":
-            self.stepCounter += 1
+            self.__scans += 1
+        self.__reward += reward
+        self.stepCounter += 1
         self.check_new_state()
 
         self.environmentLog.debug("Executed Action %s Pos: (%.2f / %.2f) Reward: %d" % (action, x, y, reward))
 
-        self.currentState["isScanned"] = 1 if self.cell_map[x, y] else 0
+        self.currentState["isScanned"] = self.cell_map[posx, posy]
+        # self.currentState["isScanned"] = self.check_range()
 
         episodeFinished = self._checkEpisodeFinished()
-        terminalState = self.currentState if episodeFinished else None
 
         if episodeFinished:
-#            reward = 100
-            self.environmentLog.info("Episode %d lasted for %d steps" % (self.episodeCounter, self.stepCounter))
+            #reward = 100 if self.discovered_percentage >= self.configDict["percentCleared"] else -100
+            reward = self.discovered_percentage ** 3 / 10000
+            self.environmentLog.info("Episode %d lasted for %d steps; reward=%d; cleared=%d" % (self.episodeCounter, self.stepCounter, reward, self.__percent_cleared))
             self.episodeLengthObservable.addValue(self.episodeCounter,
                                                   self.stepCounter)
-            self.returnObservable.addValue(self.episodeCounter,
-                                           -self.stepCounter)
+            self.returnObservable.addValue(self.episodeCounter, self.__reward)
+            self.scanObservable.addValue(self.episodeCounter, self.__scans)
             self.stepCounter = 0
             self.episodeCounter += 1
             self.currentState = self.getInitialState()
             self.resetTable()
-            self.trajectoryObservable.addTransition(previousState, action,
-                                                    reward, terminalState,
-                                                    episodeTerminated=episodeFinished)
-        else:
-            self.trajectoryObservable.addTransition(previousState, action,
-                                                    reward, self.currentState,
-                                                    episodeTerminated=episodeFinished)
+
+        self.trajectoryObservable.addTransition(previousState, action,
+                                                reward, self.currentState,
+                                                episodeTerminated=episodeFinished)
         return {
             "reward": reward,
-            "terminalState": terminalState,
+            "terminalState": self.currentState if episodeFinished else None,
             "nextState": self.currentState,
             "startNewEpisode": episodeFinished
         }
@@ -236,15 +266,24 @@ class GazeboScanTableEnvironment(SingleAgentEnvironment):
         self.__rows = None
         self.__cols = None
         self.__percent_cleared = 0.0
+        self.__reward = 0
+        self.__scans = 0
         self.pan = 0
         self.tilt = 0.5
+        self.__actions.move_cam(pan=self.pan, tilt=self.tilt)
 
     @property
     def discovered_percentage(self):
-        if self.__percent_cleared is None or self.__need_update:
-            response = self.map_percent_cleared()
-            self.__percent_cleared = response.percent * 100
-            self.environmentLog.debug("Discovered Percent: %.2f" % self.__percent_cleared)
+        c = 0.
+        for y in range(self.columns):
+            for x in range(self.rows):
+                if self.__cell_map[x, y]:
+                    c += 1.
+        self.__percent_cleared = (c / (self.columns * self.rows)) * 100
+        # if self.__percent_cleared is None or self.__need_update:
+        #     response = self.map_percent_cleared()
+        #     self.__percent_cleared = response.percent * 100
+        #     self.environmentLog.debug("Discovered Percent: %.2f" % self.__percent_cleared)
         return self.__percent_cleared
 
     @property
@@ -254,42 +293,42 @@ class GazeboScanTableEnvironment(SingleAgentEnvironment):
             self.__cell_map = numpy.ndarray(shape=(Map.num_of_cells, Map.num_of_cells), dtype=bool)
             for y in range(Map.num_of_cells):
                 for x in range(Map.num_of_cells):
-                    self.__cell_map[x, y] = self.__map.field[y * Map.num_of_cells + x].state != Cell.Unknown
+                    if x < 24 and y < 24:
+                        self.__cell_map[x, y] = True
+                    else:
+                        self.__cell_map[x, y] = self.__map.field[y * Map.num_of_cells + x].state != Cell.Unknown
         return self.__cell_map
 
     @property
     def camera_index(self):
         # Transform the pan and tilt into global coordinates
-        if self.__update_index:
-            c = PoseStamped()
-            c.header.frame_id = "sdepth"
-            c.pose.orientation.w = 1
-            cx = deepcopy(c)
-            cx.pose.position.x = 1
-            c = self.__transform_to(pose=c).pose
-            cx = self.__transform_to(pose=cx).pose
+        c = PoseStamped()
+        c.header.frame_id = "sdepth"
+        c.pose.orientation.w = 1
+        cx = deepcopy(c)
+        cx.pose.position.x = 1
+        c = self.__transform_to(pose=c).pose
+        cx = self.__transform_to(pose=cx).pose
 
-            r = (cx.pose.position.x - c.pose.position.x,
-                 cx.pose.position.y - c.pose.position.y,
-                 cx.pose.position.z - c.pose.position.z)
-            s = float(-c.pose.position.z) / r[2]
-            (x,y) = (c.pose.position.x + s * r[0], c.pose.position.y + s * r[1])
+        r = (cx.pose.position.x - c.pose.position.x,
+             cx.pose.position.y - c.pose.position.y,
+             cx.pose.position.z - c.pose.position.z)
+        s = float(-c.pose.position.z) / r[2]
+        (x,y) = (c.pose.position.x + s * r[0], c.pose.position.y + s * r[1])
 
-            if self.__map is None:
-                cell_map = self.cell_map
-            y, x = self.__map.coordinates_to_index(x,y)
-            self.__update_index = False
-            if x < 0:
-                x = 0
-            elif x >= self.rows:
-                x = self.rows - 1
-            if y < 0:
-                y = 0
-            elif y >= self.columns:
-                y = self.columns -1
-            self.x = x
-            self.y = y
-        return self.x, self.y
+        if self.__map is None:
+            cell_map = self.cell_map
+        y, x = self.__map.coordinates_to_index(x,y)
+        self.__update_index = False
+        if x < 0:
+            x = 0
+        elif x >= self.rows:
+            x = self.rows - 1
+        if y < 0:
+            y = 0
+        elif y >= self.columns:
+            y = self.columns -1
+        return x, y
 
     @property
     def cell_size(self):
