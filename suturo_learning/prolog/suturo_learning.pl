b90cd4616cfac9f87a2edd242fab147556dec644
pmania <pmania@tzi.de>
2015-06-12 15:33:23 +0200

    added another variant of goal planning retrieval

--- suturo_learning/prolog/suturo_learning.pl
+++ suturo_learning/prolog/suturo_learning.pl
@@ -18,6 +18,34 @@ get_designator_by_type(Type, Designator) :-
 get_planned_goals(Goals) :-
   Goals = [['top_grab','red_cube'], ['place_in_zone',''],['open_gripper',''],['side_grab','blue_handle'],['place_in_zone',''],['turn','']].
 
-% assert_outcome_last_plan(P) :-
-%   assert( outcome_plan(
+
+% Generate multiple plans
+goal_list_generator(Goals):-
+  Goals = [['top_grab','red_cube'], ['place_in_zone',''],['open_gripper',''],['side_grab','blue_handle'],['place_in_zone',''],['turn','']].
+
+goal_list_generator(Goals):-
+  Goals = [['top_grab','red_cube'], ['place_in_zone',''],['open_gripper',''],['top_grab','blue_handle'],['place_in_zone',''],['turn','']].
+
+% TODO Construct goal list
+get_planned_goals_w_alt(Goals) :-
+  goal_list_generator(Goals),
+  \+ failed_goal_list(Goals).
+%
+%   Make procedures dynamically known. Dirty hack :(
+:- assert(failed_goal_list([])).
+:- assert(successful_goal_list([])).
+
+% Remember which goal lists have been succesful
+assert_outcome_for_goal_list(Goals,Outcome) :-
+  Outcome = 'success' ->
+    assert(successful_goal_list(Goals));
+    % else
+    assert(failed_goal_list(Goals)).
+
+clear_asserts:-
+  retractall(successful_goal_list(_)),
+  retractall(failed_goal_list(_)),
+  assert(failed_goal_list([])),
+  assert(successful_goal_list([])).
+
 
