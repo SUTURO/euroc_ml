eb496c3064b77c94690e34b8c2d9df5bc24980a6
pmania <pmania@tzi.de>
2015-08-01 17:48:19 +0200

    modified get_learningactions_in_experiment - using only one experiment

--- suturo_learning/scripts/visualize_logs.py
+++ suturo_learning/scripts/visualize_logs.py
@@ -14,7 +14,97 @@
 
 import roslib; roslib.load_manifest('json_prolog')
 import rospy
+import rospkg
 from json_prolog import json_prolog
+
+stuff_to_write="""
+	experiments = [ 
+		
+		{ name: 'default', 
+		nodesArray: [], 
+		edgesArray : [], 
+		nodeInformationArray: [],
+		},
+
+		{ name: 'exp-foo', 
+		nodesArray: [{id: 1, label: 'LOL State'},
+    {id: 2, label: 'State 2'},
+    {id: 3, label: 'State 3'},
+    {id: 4, label: 'State 4'},
+    {id: 5, label: 'State 5'},
+    {id: 6, label: 'Error',   shape: 'diamond', color: 'red'},
+    {id: 7, label: 'Success', shape: 'diamond', color: 'green'},
+		], 
+		edgesArray : [
+    {from: 1, to: 3, arrows:'to', label:'Action', font: {align: 'top'}},
+    {from: 1, to: 2, arrows:'to', label:'Action', font: {align: 'top'}},
+    {from: 2, to: 4, arrows:'to', label:'Action', font: {align: 'top'}},
+    {from: 2, to: 5, arrows:'to', label:'Action', font: {align: 'top'}},
+    {from: 5, to: 6, arrows:'to', label:'Outcome', font: {align: 'top'}},
+    {from: 4, to: 7, arrows:'to', label:'Outcome', font: {align: 'top'}},
+		],
+		nodeInformationArray: [
+			"exp-foo-1",
+			"exp-foo-2",
+			"exp-foo-3",
+			"exp-foo-4",
+			"exp-foo-5",
+			"exp-foo-6",
+			"exp-foo-7",
+		],
+		},
+
+		];
+"""
+
+def js_representation_for_experiment(ExperimentObject):
+    """Recreate the js object from the given data, to write it to the visualization webpage sourcecode"""
+        # self.name = name
+        # self.states = states
+        # self.actions = actions
+    result = """ { name: '"""+ExperimentObject.name+"""', 
+		nodesArray: [""";
+    
+    state_id = 1
+    for s in ExperimentObject.states:
+        # print s
+        result += "{id: " + str(state_id) + ", label: '" + str(s) + "'},"
+        state_id+=1
+    # {id: 1, label: 'LOL State'},
+    # {id: 2, label: 'State 2'},
+    # {id: 3, label: 'State 3'},
+    # {id: 4, label: 'State 4'},
+    # {id: 5, label: 'State 5'},
+    # {id: 6, label: 'Error',   shape: 'diamond', color: 'red'},
+    # {id: 7, label: 'Success', shape: 'diamond', color: 'green'},
+
+    result +=	"""], 
+		edgesArray : [""";
+
+    action_id = 1
+    for a in ExperimentObject.actions:
+        result += "{from: " + str(action_id) + ", to: " + str(action_id+1) + ", arrows:'to', label:'"+ str(a) +", font: {align: 'top'}},"
+    # {from: 1, to: 3, arrows:'to', label:'Action', font: {align: 'top'}},
+    # {from: 1, to: 2, arrows:'to', label:'Action', font: {align: 'top'}},
+    # {from: 2, to: 4, arrows:'to', label:'Action', font: {align: 'top'}},
+    # {from: 2, to: 5, arrows:'to', label:'Action', font: {align: 'top'}},
+    # {from: 5, to: 6, arrows:'to', label:'Outcome', font: {align: 'top'}},
+    # {from: 4, to: 7, arrows:'to', label:'Outcome', font: {align: 'top'}},
+    result += """ ],
+		nodeInformationArray: [""";
+    result += """
+			'exp-foo-1',
+			'exp-foo-2',
+			'exp-foo-3',
+			'exp-foo-4',
+			'exp-foo-5',
+			'exp-foo-6',
+			'exp-foo-7',
+                        """;
+    result += """ ],
+		},
+                """;
+    return result;
 
 class Experiment(object):
     """docstring for Experiment"""
@@ -45,7 +135,7 @@ def load_data():
 
     # Load unique RobotExperiment IDs
     for solution in query.solutions():
-        print 'Found solution. Experiment= %s' % (solution['Experiment'])
+        print 'Found Experiments. Experiment= %s' % (solution['Experiment'])
         experimentNames.append(solution['Experiment'])
     query.finish() 
 
@@ -69,6 +159,28 @@ def load_data():
 
     return experimentObjects
 
+def write_visualization(ExperimentObjects):
+    # Load the website template 
+    rospack = rospkg.RosPack()
+    path = rospack.get_path('suturo_learning')
+    template_path = path + "/log_visualization/template.html"
+
+    template_file = open(template_path,'r')
+    template_content = template_file.read()
+    template_file.close()
+
+    visualization_content = template_content.replace("--PLACEHOLDER_REPLACE_WITH_ACTUAL_DATA--", stuff_to_write)
+    path = rospack.get_path('suturo_learning')
+    visualization_path = path + "/log_visualization/index.html"
+    visualization_file = open(visualization_path,'w')
+    visualization_file.write(visualization_content)
+    visualization_file.close()
+
+    data_to_write = "FOOBAR"
+
+
+    pass
+
 if __name__ == '__main__':
     try:
         print "hi"
@@ -76,6 +188,8 @@ if __name__ == '__main__':
         data = load_data()
         for experiment in data:
             print experiment
+            print js_representation_for_experiment(experiment)
+        write_visualization(data)
         # prolog = json_prolog.Prolog()
         # query = prolog.query("suturo_learning:l_get_robot_experiment(Experiment), suturo_learning:get_robot_experiment_name(Experiment,ExperimentName)")
         # for solution in query.solutions():
