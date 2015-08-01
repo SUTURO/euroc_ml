e0de30825d65ce093432d5f08faeb93bd999139e
stelter <stelter@tzi.de>
2015-08-01 20:10:44 +0200

    moinmoin

--- suturo_head_learner/src/suturo_head_learner.py
+++ suturo_head_learner/src/suturo_head_learner.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# from gazebo_msgs.msg._ContactState import ContactState
 from collections import defaultdict
 import rospy
 from suturo_head_mover_msgs.msg._SuturoMlAction import SuturoMlAction
@@ -9,6 +8,7 @@ from suturo_head_mover_msgs.srv._SuturoMlNextAction import SuturoMlNextAction, S
     SuturoMlNextActionResponse
 from learner import SarsaLambdaLearner
 from policy import GreedyPolicy
+from json_prolog import Prolog
 from policy import EpsilonGreedyPolicy
 
 __author__ = 'ichumuh'
@@ -16,16 +16,25 @@ __author__ = 'ichumuh'
 class SuturoMlHeadLearner(object):
 
     def __init__(self):
-        self.feedSrv = rospy.Service('SuturoMlHeadLearnerPolicyFeeder', SuturoMlFeedPolicy, self.feedCallback)
         self.feedSrv = rospy.Service('SuturoMlHeadNextAction', SuturoMlNextAction, self.nextActionCallback)
         self.policy = []
         self.q = defaultdict(lambda : 10)
         self.learner = SarsaLambdaLearner()
         self.actions = filter(lambda x: x[0].startswith('Const'), [(a,s) for a,s in vars(SuturoMlAction).iteritems()])
         self.policyMaker = EpsilonGreedyPolicy(self.q, self.actions, .0)
+        self.prolog = Prolog()
         print("SuturoMlHeadLearnerPolicyFeeder started.")
 
-    def feedCallback(self, feedPolicyRequest):
+
+    def nextActionCallback(self, nextActionRequest):
+        r = SuturoMlNextActionResponse()
+        r.action.actionId = self.policyMaker.getNextAction(nextActionRequest.state)
+        return r
+
+    def doTheShit(self):
+        q = self.prolog.query("member(A,[a,b,c,s])")
+        for sol in q.solutions():
+            print sol
         print("start learning")
         self.q = self.learner.learn(feedPolicyRequest)
         self.policyMaker.updateQ(self.q)
@@ -36,18 +45,11 @@ class SuturoMlHeadLearner(object):
         response.success = True
         return response
 
-    def nextActionCallback(self, nextActionRequest):
-        r = SuturoMlNextActionResponse()
-        r.action.actionId = self.policyMaker.getNextAction(nextActionRequest.state)
-        return r
-
-
-
-
 
 if __name__ == '__main__':
     rospy.init_node("SuturoMLHeadLearnerPolicyFeederNode", anonymous=True)
     # contact_sub = rospy.Subscriber("/gazebo/contacts", SuturoMlFeedPolicy, callback)
     # s = rospy.Service('SuturoMLHeadLearnerPolicyFeeder', CheckContact, muh)
     muh = SuturoMlHeadLearner()
+    muh.doTheShit()
     rospy.spin()
