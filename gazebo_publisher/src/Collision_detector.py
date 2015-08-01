0c89fae0b77f43f57e86686169722f4ecd09b6b6
stelter <stelter@tzi.de>
2015-08-01 13:17:11 +0200

    added new serves

--- gazebo_publisher/src/Collision_detector.py
+++ gazebo_publisher/src/Collision_detector.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # from gazebo_msgs.msg._ContactState import ContactState
 from gazebo_msgs.msg._ContactsState import ContactsState
-from suturo_head_mover_msgs.srv import CheckContact, CheckContactResponse
 import rospy
+from suturo_head_mover_msgs.srv._SuturoMlCheckContact import SuturoMlCheckContact, SuturoMlCheckContactResponse
 
 __author__ = 'ichumuh'
 
@@ -10,31 +10,31 @@ def muh(req):
     # a = ContactsState()
     global contacts
     print req
-    if contacts.empty():
+    if len(contacts) != 0:
         for contact in contacts:
             for c in contact.states:
                 # c = ContactState()
-                print c.collision1_name
-                print c.collision2_name
+                # print c.collision1_name
+                # print c.collision2_name
                 if req.object1 in c.collision1_name and req.object2 in c.collision2_name or\
                         req.object2 in c.collision1_name and req.object1 in c.collision2_name:
-                    contacts = []
-                    return CheckContactResponse(True)
+                    # contacts = []
+                    return SuturoMlCheckContactResponse(True)
     else:
         print "ERROR: didn't reseive contacts yet."
-    return CheckContactResponse(False)
+    return SuturoMlCheckContactResponse(False)
 
 
 contacts = []
-def contact_ballback(data):
+def contact_callback(data):
     global contacts
     contacts.append(data)
-    if len(contacts) > 1000:
-        pass
+    if len(contacts) > 2000:
+        contacts.pop(0)
 
 if __name__ == '__main__':
     rospy.init_node("Contactdetector", anonymous=True)
-    contact_sub = rospy.Subscriber("/gazebo/contacts", ContactsState, contact_ballback)
-    s = rospy.Service('Contactdetector', CheckContact, muh)
+    contact_sub = rospy.Subscriber("/gazebo/contacts", ContactsState, contact_callback)
+    s = rospy.Service('Contactdetector', SuturoMlCheckContact, muh)
 
     rospy.spin()
